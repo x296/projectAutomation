21b86772f27394d3c6a0a7a14cf79b09b8534bf5
Piotr Żyziński <33286821+x296@users.noreply.github.com>
2020-04-26 11:17:27 +0200

    initialization of a new repo

--- .my_commands.sh
+++ .my_commands.sh
@@ -3,5 +3,11 @@
 function create() {
 	python3 /Users/piotrzyzinski/pythonProjects/projectAutomation/create.py $1
 	cd /Users/piotrzyzinski/pythonProjects/$1
-	# git init
+	git init
+	git remote add origin https://github.com/x296/$1.git
+	touch README.md
+	git add .
+	git commit -m 'initialization of a new repo'
+	git push -u origin master
+	subl .
 }
--- create.py
+++ create.py
@@ -3,25 +3,53 @@ import sys
 import time
 from selenium import webdriver
 
-driver = webdriver.Safari()
-driver.get('https://github.com/login')
-login_field = driver.find_element_by_xpath('//*[@id="login_field"]')
-login_field.clear()
-login_field.send_keys('login')
-password_field = driver.find_element_by_xpath('//*[@id="password"]')
-password_field.clear()
-password_field.send_keys('password')
-log_in_btn = driver.find_element_by_xpath('//*[@id="login"]/form/div[4]/input[9]')
-log_in_btn.click()
-time.sleep(3)
-driver.get('https://github.com/new')
-new_repo_name_field = driver.find_element_by_xpath('//*[@id="repository_name"]')
-new_repo_name_field.send_keys(sys.argv[1])
-time.sleep(2)
+path = '/Users/piotrzyzinski/pythonProjects/'
+folderName = str(sys.argv[1]) # Linking folder name sending by arg to string
+driver = webdriver.Safari() # Defining driver for Safari
+file = open('files/git_pass.txt', 'r')
+if f.mode == 'r':
+	git_login = str(file.readlines())
+	git_pass = str(file.readlines())
+	
+file.close()
+
+
+print(git_login)
+print(git_pass)
 
 def create():
 	try: 
-		os.makedirs('/Users/piotrzyzinski/pythonProjects/' + sys.argv[1])
+		# Creating dir with arg from command	
+		os.makedirs(path + folderName)
+
+		# Filling out login form
+		driver.get('https://github.com/login')
+		
+		login_field = driver.find_element_by_xpath('//*[@id="login_field"]')
+		login_field.clear()
+		login_field.send_keys('piotr.zyzinski@gmail.com')
+		
+		password_field = driver.find_element_by_xpath('//*[@id="password"]')
+		password_field.clear()
+		password_field.send_keys('raxzoj-5zutga-qIbdep')
+		
+		log_in_btn = driver.find_element_by_xpath('//*[@id="login"]/form/div[4]/input[9]')
+		log_in_btn.click()
+		
+		time.sleep(3) # Probably my internet is slow and i have to wait until page will be fully loaded
+		
+		# Creating new repo with arg from command
+		driver.get('https://github.com/new')
+		new_repo_name_field = driver.find_element_by_xpath('//*[@id="repository_name"]')
+		new_repo_name_field.send_keys(sys.argv[1])
+		
+		time.sleep(2) # Waiting for check repo's name availability
+		
+		new_repo_btn = driver.find_element_by_css_selector('button.first-in-line')
+		new_repo_btn.submit()
+		
+		time.sleep(3) # Waiting for submit data
+		driver.close()
 
 	except Exception as error:
 		print(error)
